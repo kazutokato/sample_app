f07e914b2bcbb0072298fceec8c9f521041904b1
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-14 10:54:40 +0000

    [Start] 2-8

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_tudolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolist/:id' => 'todolists#destroy', as: 'destroy_todolist'
 end
