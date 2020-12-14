d8c929e07e480c4e42d50d0a070623b5969fb3cc
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-14 10:50:09 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '60d1efb3f6ec88fa3cbabbbfd8e105913aad73f087888f0dd04ad46ef7c0122fde29f4992c29fcaaa888b2a111efc2aff75f770fe65066f96dd60591c4dc837c'
